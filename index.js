f1b8ddba056fc7a2a31fefaee43f0412f72faae1
Anh Nhut <lanhut3926@gmail.com>
2019-04-12 00:04:50 +0700

    Query & POST

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
 const port = 3000
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+//dody parser
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+//
+
+var citys = [
+    {name:'Long Xuyen' }, // name cua input text phai giong voi key o day
+    {name :'HCM City'}
+];
 
 //app.get('/testnodemon', (req, res) => res.send('Hello World ! fxxk le van dat' ))  // arrow
 
 app.get('/testnodemon',function(req , res){
-   
+   // testnodemon se hien thi file html citys.pug
     res.render('city/citys.pug',{
-        city: [
-            {name:'Long Xuyen' },
-            {name :'HCM City'}
-        ]
+        city: citys
     });
 });
 
@@ -24,4 +31,28 @@ app.get('/',function(req , res){
          
     }); // trong localhost:3000 render ra file index.pug
 });
+// query parameter
+app.get('/city/search',function(req,res){
+    //console.log(req.query); // khi nhap ?q=th 
+   var q = req.query.q;
+   var matchedcity= citys.filter(function(item){
+        return item.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+   });
+   res.render('city/citys.pug',{
+    city: matchedcity // render vao file html city/citys.pug va chi hien ra nhung thang duoc filter
+    });
+});
+//POST
+//tao duong dan moi & form de add them city
+app.get('/city/create',function(req,res){
+    res.render('city/create.pug');//file html create.pug
+});
+app.post('/city/create',function(req,res){
+   // console.log(req.body);
+   citys.push(req.body);
+   res.redirect('/testnodemon');
+
+});
+
 app.listen(port, () => console.log(`Deployed ${port}!`))
+// duong dan toi file hmtl dang pug
